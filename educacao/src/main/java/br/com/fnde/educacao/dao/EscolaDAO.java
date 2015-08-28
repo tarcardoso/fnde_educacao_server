353ae0fa6764e2d6b520c03ad318764611c0d3d1
tarcardoso <tarcisio.cardoso@bluebile.com>
2015-08-28 10:27:21 -0300

    Ajuste para geofance.
    Paginacao ainda incompleta

--- educacao/src/main/java/br/com/fnde/educacao/dao/EscolaDAO.java
+++ educacao/src/main/java/br/com/fnde/educacao/dao/EscolaDAO.java
@@ -3,9 +3,11 @@ package br.com.fnde.educacao.dao;
 import java.util.List;
 
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.springframework.stereotype.Repository;
 
 import br.com.fnde.educacao.domain.Escola;
+import br.com.fnde.educacao.domain.TimeLine;
 import br.com.mec.fies.core.db.HibernateDAOImpl;
 
 @Repository
@@ -18,29 +20,44 @@ public class EscolaDAO extends HibernateDAOImpl<Escola> {
 	
 	public List<Escola> getByLatitudeLongitude(double latitude, double longitude) {
 		
-		Query query = getSession().createSQLQuery(
-				" SELECT idEscola, ( (3959 * 1.609 * 1000) * acos( cos( radians( :latitude ) ) * cos( "
-						+" radians(lat ) ) * cos( "
-						+" radians( lng ) - radians( :longitude) ) + sin( radians( :latitude) ) * sin( "
-						+"  radians( lat ) ) ) ) AS "
-						+"  distance "
-						+" FROM escola where idEscola <= 245  "
-					);
-		query.setParameter("latitude", latitude);   //-15.844539);
-		query.setParameter("longitude", longitude); //-47.880803);
-		
-		List results = query.list();
-		
-		String listaId = "";
-		for (Object object : results) {
-			Object[] arr = (Object[])object;
-			listaId += arr[0]+",";
-			System.out.println( arr[0]+": "+arr[1] );
-		}
-		listaId = listaId.substring(0, listaId.length()-1 );
-		Query queryEscola = getSession().createQuery("from Escola escola where escola.idEscola in ("+listaId+") ");
-		
-		return queryEscola.list();
+		SQLQuery query = getSession().createSQLQuery(
+				" SELECT *, ( (3959 * 1.609 * 1000) * acos( cos( radians( :latitude) ) * cos(  "
+						+" radians(numlatitude ) ) * cos( "
+						+" radians( numlongitude ) - radians( :longitude) ) + sin( radians( :latitude) ) * sin(  " 
+						+" radians( numlatitude ) ) ) ) AS "
+						+" distance "
+						+" FROM escola order by distance limit 10"
+						);
+			query.setParameter("latitude", latitude); //-15.844539);
+			query.setParameter("longitude", longitude); //-47.880803);
+			
+			query.addEntity(Escola.class);
+			
+			return query.list();
+			
+//		Query query = getSession().createSQLQuery(
+//				" SELECT idEscola, ( (3959 * 1.609 * 1000) * acos( cos( radians( :latitude ) ) * cos( "
+//						+" radians(lat ) ) * cos( "
+//						+" radians( lng ) - radians( :longitude) ) + sin( radians( :latitude) ) * sin( "
+//						+"  radians( lat ) ) ) ) AS "
+//						+"  distance "
+//						+" FROM escola where idEscola <= 245  "
+//					);
+//		query.setParameter("latitude", latitude);   //-15.844539);
+//		query.setParameter("longitude", longitude); //-47.880803);
+//		
+//		List results = query.list();
+//		
+//		String listaId = "";
+//		for (Object object : results) {
+//			Object[] arr = (Object[])object;
+//			listaId += arr[0]+",";
+//			System.out.println( arr[0]+": "+arr[1] );
+//		}
+//		listaId = listaId.substring(0, listaId.length()-1 );
+//		Query queryEscola = getSession().createQuery("from Escola escola where escola.idEscola in ("+listaId+") ");
+//		
+//		return queryEscola.list();
 	}
 	public List<Escola> getTodos() {
 		return recuperarTodos();
--- educacao/src/main/java/br/com/fnde/educacao/dao/TimeLineDAO.java
+++ educacao/src/main/java/br/com/fnde/educacao/dao/TimeLineDAO.java
@@ -3,9 +3,9 @@ package br.com.fnde.educacao.dao;
 import java.util.List;
 
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.springframework.stereotype.Repository;
 
-import br.com.fnde.educacao.domain.Escola;
 import br.com.fnde.educacao.domain.TimeLine;
 import br.com.mec.fies.core.db.HibernateDAOImpl;
 
@@ -18,29 +18,33 @@ public class TimeLineDAO extends HibernateDAOImpl<TimeLine> {
 	}
 	
 	public List<TimeLine> getByLatitudeLongitude(double latitude, double longitude) {
-		Query query = getSession().createSQLQuery(
-				" SELECT idEscola, ( (3959 * 1.609 * 1000) * acos( cos( radians( :latitude ) ) * cos( "
-						+" radians(lat ) ) * cos( "
-						+" radians( lng ) - radians( :longitude) ) + sin( radians( :latitude) ) * sin( "
-						+"  radians( lat ) ) ) ) AS "
-						+"  distance "
-						+" FROM escola where idEscola <= 245  "
+		//TODO FAZER LOGIDA PARA PAGINACAO
+		SQLQuery query = getSession().createSQLQuery(
+			" select *,  "
+			+" 	(select (3959 * 1.609 * 1000) * acos( cos( radians( :latitude) ) * cos(  "   
+			+" 	radians(numlatitude ) ) * cos( "
+			+" 	radians( numlongitude ) - radians( :longitude) ) + sin( radians( :latitude) ) * sin(   "
+			+" 	 radians( numlatitude ) ) )  "
+			+" 	FROM escola where idEscola = tl.idEscola ) AS distancia "
+			+"  from timeline tl order by distancia limit 10"
 					);
 		query.setParameter("latitude", latitude); //-15.844539);
 		query.setParameter("longitude", longitude); //-47.880803);
 		
-		List results = query.list();
+		query.addEntity(TimeLine.class);
 		
-		String listaId = "";
-		for (Object object : results) {
-			Object[] arr = (Object[])object;
-			listaId += arr[0]+",";
-			System.out.println( object);
-		}
-		listaId = listaId.substring(0, listaId.length()-1 );
-		Query queryEscola = getSession().createQuery("from TimeLine tl where tl.idEscola in ("+listaId+") ");
+		return query.list();
 		
-		return queryEscola.list();
+//		String listaId = "";
+//		for (Object object : results) {
+//			Object[] arr = (Object[])object;
+//			listaId += arr[0]+",";
+//			System.out.println( object);
+//		}
+//		listaId = listaId.substring(0, listaId.length()-1 );
+//		Query queryEscola = getSession().createQuery("from TimeLine tl where tl.idEscola in ("+listaId+") ");
+//		
+//		return queryEscola.list();
 	}
 	
 	public List<TimeLine> getTodos() {
